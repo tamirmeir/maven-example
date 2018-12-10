0f29dab34162b7a0371f531ff3ce863710049b3b
Eli Shemesh <eli.shemesh@algosec.com>
2018-12-10 05:59:30 +0200

    test fix

--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class OtherTest {
+public class Test01 {
 
     @Test public void mytest() {
         String str = "my string";
