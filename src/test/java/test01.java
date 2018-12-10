8ac561101e7ed9d15fa8815f284c46d99b8fef86
Eli Shemesh <eli.shemesh@algosec.com>
2018-12-10 05:55:43 +0200

    initial commit

---
+++ src/test/java/test01.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class OtherTest {
+
+    @Test public void mytest() {
+        String str = "my string";
+	assertEquals("my string", str);
+    }
+}
