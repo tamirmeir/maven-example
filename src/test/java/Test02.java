075d4cbb92b3ae62d4f5a9b70dff798816c5e801
tamirmeir <4tamirmeir@gmail.com>
2018-12-12 18:30:21 +0200

    Create Test02.java

---
+++ src/test/java/Test02.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Test02 {
+
+    @Test public void mytest() {
+        String str = "my string";
+	assertEquals("my string1", str);
+    }
+}
